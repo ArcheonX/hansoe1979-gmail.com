babc41d668b8c40688ca3fef3b8290eb07aebe99
winai <winai@lannasoftworks.com>
2020-04-10 22:48:36 +0700

    set git

--- SQL/ResetUserPassword.sql
+++ SQL/ResetUserPassword.sql
@@ -1,5 +1,9 @@
 alter table LDMS_M_UserRole
-add [passwordSalt] nvarchar(250); 
+alter column [passwordSalt] nvarchar(1024); 
+alter table LDMS_M_UserRole
+alter column [passwordSalt] nvarchar(1024); 
 
 update LDMS_M_UserRole
 set [Password]='rQs8kl/WhNIouVUSK6YvXjzeXKA=' , [passwordSalt]='H/O9luM+jQyw86vscWcpRA==';
+
+ 
