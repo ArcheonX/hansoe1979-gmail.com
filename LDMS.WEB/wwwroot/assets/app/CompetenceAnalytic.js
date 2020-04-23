8b9cccb926df8fe3de23fe9165e482c65a502855
winai <winai@lannasoftworks.com>
2020-04-23 21:38:55 +0700

    Merge branch 'features/Platform' into develop

--- LDMS.WEB/wwwroot/assets/app/CompetenceAnalytic.js
+++ LDMS.WEB/wwwroot/assets/app/CompetenceAnalytic.js
@@ -1,4 +1,5 @@
-﻿(function ($) {
+﻿var topics = [];
+(function ($) {
     "use strict";
     $(document).ready(function () {
         $("input[type='datetime']").datepicker();
@@ -46,13 +47,61 @@ function CreateEmployeePopup() {
 function AddTopic(item) {
     var tbody = $('#dtTopic').children('tbody');
     var table = tbody.length ? tbody : $('#dtTopic');
-
-    var row = '<tr>' +
-        '<td style="text-align:center;width:10%"><label class="LabelCaption">1</label></td>' +
-        '<td style="text-align:center;width:80%"><input type="text"  title="Please Enter Employee ID" id="txtEmployeeId" style="width: 100%" class="form-control required" /> </td>' +
-        '<td style="text-align:center;width:10%"><button id="btnSearchEmployee" class="btn btn-outline-danger waves-effect waves-light" style="width:100%"><img style="width:35px;height:30px" src="../assets/images/svg/cancel.svg" /></button>' +
-        '</td></tr>';
-    table.append(row);
+    var index = (topics.length + 1);   
+    var text = "";
+    var data = $("#selectCourse").select2('data');
+    if (data && data.length > 0 && data[0].id!="") {        
+        text = data[0].text;
+        $('select[name="selectCourse"]').val(null).trigger('change');
+    } else {
+        text = $("#txtSpecialTopic").val();
+        $("#txtSpecialTopic").val("");
+    }
+    var topic = {
+        Index: index,
+        Text: text
+    };
+    topics.push(topic);
+     
+    //$('#dtTopic').DataTable({
+    //    data: topics,
+    //    destroy: true,
+    //    'columns': [
+    //        { data: 'Index' },
+    //        { data: 'Text' }]
+    //});
+    table.empty();
+    $.each(topics, function () {
+        var row = '<tr>' +
+            '<td style="text-align:center;width:10%"><label class="LabelCaption">' + this.Index + '</label></td>' +
+            '<td style="text-align:center;width:80%"><input type="text" id="txtTopic_"' + this.Index + ' style="width: 100%" value="' + this.Text + '" disabled class="form-control required" /> </td>' +
+            '<td style="text-align:center;width:10%"><button id="btnRemoveTopic_"' + this.Index + '" class="btn btn-outline-danger waves-effect waves-light" style="width:100%"><img style="width:35px;height:30px" src="../assets/images/svg/cancel.svg" /></button>' +
+            '</td></tr>';
+        table.append(row);
+    });
+    //$('#dtTopic').DataTable().destroy();
+    //$('#dtTopic').DataTable({
+    //    "autoWidth": false,
+    //    "lengthChange": false,
+    //    "pageLength": 10,
+    //    "paging": true,
+    //    "ordering": false,
+    //    "searching": false,
+    //    "lengthChange": false,
+    //    "bAutoWidth": false,
+    //    "Filter": true,
+    //    "info": false,
+    //    "bPaginate": false,
+    //    "bLengthChange": false,
+    //    "language": {
+    //        "lengthMenu": "Display _MENU_ records per page",
+    //        "zeroRecords": "    Nothing found - sorry",
+    //        "info": "    page _PAGE_ of _PAGES_",
+    //        "infoEmpty": "    No records available",
+    //        "infoFiltered": "(filtered from _MAX_ total records)"
+    //    }
+    //});
+    $('.dataTables_length').addClass('bs-select');
 }
 function LoadCourse() {
     var options = $('#selectCourse');
@@ -204,19 +253,21 @@ function LoadDepartmentSection(departmentId) {
         }
     });
 }
-function SearchEmployee() {
-    var table = $('#dtEmployee').DataTable();
-    table.destroy();
+function SearchEmployee() {  
     var searmodel = {
-        EmployeeId: "",
-        EmployeeName: "",
-        Departments: "",
-        Sections: "",
-        JobGrades: "",
-        JobTitles: "",
+        EmployeeId: $("#txtEmployeeID").val(),
+        EmployeeName: $("#txtEmployeeName").val(),
+        Departments: $("#selectDepartment").val(),
+        Sections: $("#selectSection").val(),
+        JobGrades: $("#selectJobGrade").val(),
+        JobTitles: $("#selectJobTitle").val(),
         Levels: "",
     }
     MessageController.BlockUI({ boxed: true, target: '#search-target-employee-modal' });
+
+    var table = $('#dtEmployee').DataTable();
+    table.destroy();
+
     $.ajax({
         type: "GET",
         url: "/Employee/SearchEmployee",
