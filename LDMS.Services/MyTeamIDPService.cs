f07edb692885233b77c3ad79831922153c61e003
hansoe1979@gmail.com <62320097+ArcheonX@users.noreply.github.com>
2020-05-02 04:29:41 +0700

    Update MyTeamIDPService.cs

--- LDMS.Services/MyTeamIDPService.cs
+++ LDMS.Services/MyTeamIDPService.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Dapper;
+using LDMS.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using LDMS.Daos;
+using LDMS.ViewModels;
 
 namespace LDMS.Services
 {
-    class MyTeamIDPService
+    public class MyTeamIDPService : ILDMSService
     {
+        private readonly ILogger<CoachingService> _logger;
+        public MyTeamIDPService(
+           ILogger<CoachingService> logger,
+           ILDMSConnection iLDMSConnection, IHttpContextAccessor httpContextAccessor) : base(iLDMSConnection, httpContextAccessor)
+        {
+            _logger = logger;
+        }
+
+        //public ViewModels.
     }
 }
