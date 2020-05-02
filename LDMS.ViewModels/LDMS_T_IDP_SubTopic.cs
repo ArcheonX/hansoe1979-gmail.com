714cf9b4f7cf836acdd619bf58b544f89b60a8a3
Pisit Imlerthiran <pisit10@gmail.com>
2020-05-03 01:21:20 +0700

    add idp update coaching

--- LDMS.ViewModels/LDMS_T_IDP_SubTopic.cs
+++ LDMS.ViewModels/LDMS_T_IDP_SubTopic.cs
@@ -6,10 +6,13 @@ namespace LDMS.ViewModels
     {
         public int ID { get; set; }
         public int ID_IDP_Topic { get; set; }
+        public string DevelopementTopic { get; set; }
+        public string TopicExpectedOutcom { get; set; }
         public string HowToDevelopment { get; set; }
         public string ExpectedOutcom { get; set; }
         public DateTime ByWhen { get; set; }
         public int SubTopicStatus { get; set; }
+        public string SubTopicStatusName { get; set; }
         public string CreateBy { get; set; }
         public DateTime CreateDate { get; set; }
         public string UpdateBy { get; set; }
