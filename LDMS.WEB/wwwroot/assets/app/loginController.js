af302b3ed0e7fb56c60233c8b2269639cf2c90b9
winai <winai@lannasoftworks.com>
2020-05-02 23:58:27 +0700

    menu layout

--- LDMS.WEB/wwwroot/assets/app/loginController.js
+++ LDMS.WEB/wwwroot/assets/app/loginController.js
@@ -1,13 +1,15 @@
 ﻿$(document).ready(function () { 
-    if (localStorage.RememberMe && localStorage.RememberMe != '' && localStorage.RememberMe == "true") {
-        $('#chkRememberMe').prop('checked', true);
-        $('#txtUserName').val(localStorage.usrname);
-        $('#txtPassword').val(localStorage.pass);
-    } else {
-        $('#chkRememberMe').prop('checked', false);
-        $('#txtUserName').val("");
-        $('#txtPassword').val("");
-    }
+    //if (localStorage.RememberMe && localStorage.RememberMe != '' && localStorage.RememberMe == "true") {
+    //    $('#chkRememberMe').prop('checked', true);
+    //    var usrDc = CryptoJS.AES.decrypt(CookiesController.setCookie("username") , "JHKJHKAHFKSHFKHLKHFSKLHFSKLHFKLHSFKLSFHLKHFKSHFKlhF");
+    //    var pssDc = CryptoJS.AES.decrypt(CookiesController.setCookie("passkey"), "JHKJHKAHFKSHFKHLKHFSKLHFSKLHFKLHSFKLSFHLKHFKSHFKlhF"); 
+    //    $('#txtUserName').val(usrDc.toString(CryptoJS.enc.Utf8));
+    //    $('#txtPassword').val(pssDc.toString(CryptoJS.enc.Utf8));
+    //} else {
+    //    $('#chkRememberMe').prop('checked', false);
+    //    $('#txtUserName').val("");
+    //    $('#txtPassword').val("");
+    //}
 
     $("#txtPassword").bind("keyup", function (event) {
         var code = (event.keyCode ? event.keyCode : event.which);
@@ -17,27 +19,18 @@
         }
     });
 });
-function OnAuthentication() {
-    //MessageController.BlockUI({
-    //    target: '#main-wrapper',
-    //});
+function OnAuthentication() { 
     var loginModel = {
         Username: $("#txtUserName").val(),
         Password: $("#txtPassword").val()
     };
 
     if (loginModel.Username == null || loginModel.Username == "") { 
-        MessageController.Error("Pleas Enter 'Username'", 'Login Faild');
-        //MessageController.UnblockUI({
-        //    target: '#main-wrapper',
-        //});
+        MessageController.Error("Pleas Enter 'Username'", 'Login Faild'); 
         return;
     }
     if (loginModel.Password == null || loginModel.Password == "") { 
-        MessageController.Error("Pleas Enter 'Password'", 'Login Faild');
-        //MessageController.UnblockUI({
-        //    target: '#main-wrapper',
-        //});
+        MessageController.Error("Pleas Enter 'Password'", 'Login Faild'); 
         return;
     }
     MessageController.BlockUI({ boxed: true, target: '#frm-login' });
@@ -47,16 +40,16 @@ function OnAuthentication() {
         data: loginModel,
         success: function (response) { 
             if (response.Code == 200) { 
-                if ($("#chkRememberMe").prop('checked') == true) { 
-                    localStorage.usrname = $('#txtUserName').val();
-                    localStorage.pass = $('#txtPassword').val();
-                    localStorage.RememberMe = true; 
-                } else {
-                    $('#chkRememberMe').prop('checked', false);
-                    localStorage.usrname = "";
-                    localStorage.pass = "";
-                    localStorage.RememberMe = false;
-                }
+                //if ($("#chkRememberMe").prop('checked') == true) {
+                //    CookiesController.setCookie("username") = CryptoJS.AES.encrypt($('#txtUserName').val(), "JHKJHKAHFKSHFKHLKHFSKLHFSKLHFKLHSFKLSFHLKHFKSHFKlhF");
+                //    CookiesController.setCookie("passkey") = CryptoJS.AES.encrypt($('#txtPassword').val(), "JHKJHKAHFKSHFKHLKHFSKLHFSKLHFKLHSFKLSFHLKHFKSHFKlhF"); 
+                //    localStorage.RememberMe = true; 
+                //} else {
+                //    $('#chkRememberMe').prop('checked', false);
+                //    CookiesController.setCookie("username") = null;
+                //    CookiesController.setCookie("passkey") = null;
+                //    localStorage.RememberMe = false;
+                //}
                 MessageController.UnblockUI('#frm-login');
                 window.location.href = response.Data;
             } else { 
